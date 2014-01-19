d3e3a86f2f8dc9812ca6163d1037226e4deacd20
magic282 <magic282sub@gmail.com>
2014-01-19 15:47:46 +0800

    Add files.

---
+++ ShooterSubDownloader/ShooterSubDownloader/Logger.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShooterSubDownloader
+{
+    public class Logger
+    {
+        #region Log
+        private static object LogLock = new object();
+        private static string LogFilePath = Path.GetDirectoryName(
+            System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\ShooterSubDownloaderLog.txt";
+        public static void Log(string message, EventLogEntryType type)
+        {
+            try
+            {
+                lock (LogLock)
+                {
+                    if (File.Exists(LogFilePath))
+                    {
+                        File.AppendAllText(LogFilePath, DateTime.Now.ToString() + "\t" +
+                            "[Shooter]" + ": " + message + "\r\n");
+                    }
+                    else
+                    {
+                        File.WriteAllText(LogFilePath, DateTime.Now.ToString() + "\t" +
+                             "[Shooter]" + ": " + message + "\r\n");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+        public static void Log(string message)
+        {
+            Log(message, EventLogEntryType.Information);
+        }
+        public static void Log(Exception e)
+        {
+            Log(e.Message + "\r\n" + e.StackTrace + "\r\n", EventLogEntryType.Error);
+        }
+        #endregion
+
+        public static void clear()
+        {
+            FileInfo f = new FileInfo(LogFilePath);
+            try
+            {
+                f.Delete();
+            }
+            catch
+            {
+
+            }
+
+        }
+    }
+}
